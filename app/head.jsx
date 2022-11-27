b5e01b2e08fd5d2ef2e996506528a25e217fde9f
juamatx <60664640+juamatx@users.noreply.github.com>
2022-11-26 20:13:25 -0500

    Update head.jsx

--- app/head.jsx
+++ app/head.jsx
@@ -4,6 +4,10 @@ export default function Head() {
       <title>Shareversity</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Find jobs and research opportunities" />
+      <meta http-equiv="cache-control" content="no-cache" />
+      <meta http-equiv="expires" content="0" />
+      <meta http-equiv="pragma" content="no-cache" />
+
       <link rel="icon" href="/favicon.ico" />
     </>
   );
