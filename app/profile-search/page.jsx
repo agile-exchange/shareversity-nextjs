d4b7628020a17d8874b8632ecfb0065eaad007db
Brian Wagner <39681903+BrianWagner-Github@users.noreply.github.com>
2022-12-05 17:57:41 -0500

    Profile Search Update
    Search Bar inputs for name/skill/email address. Will need to be hooked up to our DB.

--- app/profile-search/page.jsx
+++ app/profile-search/page.jsx
@@ -1,16 +1,82 @@
-// import styles from "../page.module.css";
+"use client"
+import { SupabaseAuthClient } from '@supabase/supabase-js/dist/module/lib/SupabaseAuthClient';
+import { getData } from '../../Modules/module';
+import { useState, useEffect } from 'react';
+import '../../App.css';
+import StudentCard from '../../components/StudentCard';
+import SearchInput from '../../components/SearchInput';
+import SearchTag from '../../components/SearchTag';
+import LoginUI from '../../components/login';
+
 
 export default function Search (){
+   const [data, setData] = useState([]);
+  const [nameSearchInput, setNameSearchInput] = useState('');
+  const [tagSearchInput, setTagSearchInput] = useState('');
+  const [filteredResult, setFilteredResult] = useState([]);
+  const [filteredTagResult, setFilteredTagResult] = useState([]);
+
+  useEffect(() => {
+    getData().then((data) => {
+      data.students.map((student) => (student.tags = []));
+      setData(data.students);
+    });
+  }, []);
+
+  const searchItems = (searchValue) => {
+    if (searchValue !== '') {
+      const filteredData = data.filter((student) => {
+        const name = student.firstName + student.lastName;
+        return name.toLowerCase().includes(searchValue.toLowerCase());
+      });
+      setFilteredResult(filteredData);
+    } else {
+      setFilteredResult(data);
+    }
+  };
+
+  const searchTags = (tagValue) => {
+    if (tagValue !== '') {
+      const filteredTag = [];
+
+      // eslint-disable-next-line array-callback-return
+      data.filter((student) => {
+        // eslint-disable-next-line array-callback-return
+        student.tags.map((tag) => {
+          if (tag.toLowerCase().includes(tagValue.toLowerCase())) {
+            filteredTag.push(student);
+          }
+        });
+      });
+
+      setFilteredTagResult(filteredTag);
+    } else {
+      setFilteredTagResult(data);
+    }
+  };
    return (<>
-<div className="profile_search">
-   <label>
-            <h1>Search Profiles</h1><br />
-            <input
-              type="text"
-              name="profile-search"
-            />
-          </label>
-          </div>
-   
-   </>) 
+      <SearchInput searchItems={searchItems} setNameSearchInput={setNameSearchInput} />
+      <SearchTag searchTags={searchTags} setTagSearchInput={setTagSearchInput} />
+      <div className="main">
+        <div>
+          {nameSearchInput.length > 0
+            ? filteredResult.map((student) => (
+                <div>
+                  <StudentCard key={student.id} {...student} students={data} setData={setData} />
+                </div>
+              ))
+            : tagSearchInput.length > 0
+            ? filteredTagResult.map((student) => (
+                <div>
+                  <StudentCard key={student.id} {...student} students={data} setData={setData} />
+                </div>
+              ))
+            : data.map((student) => (
+                <div>
+                  <StudentCard key={student.id} {...student} students={data} setData={setData} />
+                </div>
+              ))}
+        </div>
+      </div>
+    </>) ;
 }
---
+++ components/StudentCard.js
@@ -0,0 +1,92 @@
+import { useState } from 'react';
+import { average } from '../Modules/module';
+import '../App.css';
+
+export default function StudentCard({
+  id,
+  firstName,
+  lastName,
+  pic,
+  email,
+  company,
+  skill,
+  grades,
+  students,
+  tags,
+  setData
+}) {
+  const [open, setOpen] = useState(false);
+
+  const [tagInput, setTagInput] = useState('');
+
+  const toggleBtn = () => {
+    if (open === false) {
+      setOpen(true);
+    } else {
+      setOpen(false);
+    }
+  };
+
+  const addTagHandler = (e) => {
+    e.preventDefault();
+    const newStudents = students.map((student) => {
+      if (student.id === id && tagInput !== '' && !student.tags.includes(tagInput)) {
+        student.tags = [...student.tags, tagInput];
+      }
+      return student;
+    });
+    setData(newStudents);
+    setTagInput('');
+  };
+
+  return (
+    <div>
+      <div className="container">
+        <div className="student-card">
+          <div className="student-sub-card">
+            <div>
+              <button className="button" onClick={() => toggleBtn()}>
+                {open === false ? '+' : '='}
+              </button>
+            </div>
+            <div>
+              <img src={pic} alt="profile-img" />
+            </div>
+            <div>
+              <h1>
+                {firstName} {lastName}
+              </h1>
+              <p>Email: {email}</p>
+              <p>Company: {company}</p>
+              <p>Skill: {skill}</p>
+              <p>Average: {average(grades)}%</p>
+            </div>
+          </div>
+          <div className="tag">
+            <ul>
+              {tags.map((tag, i) => (
+                <li key={i}>{tag}</li>
+              ))}
+            </ul>
+            <form onSubmit={addTagHandler}>
+              <input
+                type="text"
+                placeholder="Add a tag"
+                className="tag-input"
+                value={tagInput}
+                onChange={(e) => setTagInput(e.target.value)}
+              />
+            </form>
+          </div>
+          <div className={open === false ? 'test-close' : 'test'}>
+            {grades.map((score, i) => (
+              <p key={i}>
+                Test{i + 1}: <span className="score">{score}%</span>
+              </p>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
